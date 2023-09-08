3aedc791445bb60a1fc1febe3c6e0f73b5e16702
U-BSD\464801 <464801@SHS-15W2NW3.beaverton.k12.or.us>
2023-09-08 12:11:27 -0700

    This is my first commit to the Guessing Game Project

---
+++ GuessingGame/Guessing_Game.cpp
@@ -0,0 +1,32 @@
+/* 
+   This program... WRITE WHAT THIS PROGRAM DOES HERE!
+   Author: Kevin Chacko
+   Period 6, C++ / Data Structures
+*/
+
+/*
+  THE 3 RULES FOR C++ CLASS:
+  1. No global variables! (Though Global constants are ok).
+  2. Never use strings! (Use cstrings or character arrays instead, and the cstring and cctype libraries are ok. Its recommended that you use strcmp and strlen in your projects).
+  3. You should include <iostream>, not stdio. Also, use "new" and "delete" instead of "malloc" and "free".
+
+  Also as for a few recommendations:
+  1. Avoid using the mouse when possible. Learn the shortcuts. If you ever work through a shell, you won't have a mouse available to you.
+  2. Only use break/continue if you have to, like in switch statements. Otherwise don't use them.
+*/
+
+/*
+  I got a lot of help with learning c++ for this project from the "Introduction to C++: Video Tutorials" module
+
+  WRITE DOWN ANY MORE RESOURCES THAT YOU USED TO HELP YOU IN THIS PROJECT!
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  //If nothing went wrong, return 0
+  return 0;
+}
