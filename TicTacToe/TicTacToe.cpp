eb90f1a3e6e9c621d32f11339122e377e9824655
U-BSD\464801 <464801@SHS-75W2NW3.beaverton.k12.or.us>
2023-10-02 12:28:17 -0700

    Looking at adding stuff to the board!

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -23,22 +23,42 @@ using namespace std;
 
 //Function prototypes
 void instructions();
-char[2] askInput(bool turn, char grid[3][3]);
+void askInput(bool turn, int & inputRow, int & inputColumn, char grid[][3]);
+void printGrid(char grid[][3]);
+
+//Global constants
+const char p1Sym = 'o';
+const char p2Sym = 'x';
+const bool p1Turn = true;
+const bool p2Turn = false;
 
 //start of main function
 int main() {
   //variable declarations
-  const char p1Sym = 'o';
-  const char p2Sym = 'x';
   bool playerTurn = true;
-  const bool p1Turn = true;
-  const bool p2Turn = false;
   char grid[3][3];
   int inputRow = 0;
   int inputColumn = 0;
+  bool playingGame = true;
   
-  //give instructions first
+  //give instructions first time playing
   instructions();
+
+  //keep them in the loop until they say that they don't want to play anymore
+  while(playingGame) {
+    //Show grid for the first time if the grid is currently clear
+    if(grid[0][0] == '\0' && grid[0][1] == '\0' && grid[0][2] == '\0'
+       && grid[1][0] == '\0' && grid[1][1] == '\0' && grid[1][2] == '\0'
+       && grid[2][0] == '\0' && grid[2][1] == '\0' && grid[2][2] == '\0') {
+      printGrid(grid);
+    }
+    
+    //Ask for player input and add it to the grid
+    askInput(playerTurn, inputRow, inputColumn, grid);
+
+    //print out board
+    printGrid(grid);
+  }
   
   //if everything turned out fine, return 0
   return 0;
@@ -61,39 +81,81 @@ void instructions() {
   cout << endl;
 }
 
-//This function asks for the player's input and validates it.
-char askInput[2] (bool turn, char grid[3][3]) {
+//This function prints out the current playing grid of the game
+void printGrid(char grid[][3]) {
+  cout << "\t|1|\t|2|\t|3|" << endl;
+  cout << "|a|\t" << grid[0][0] << "\t" << grid[0][1] << "\t" << grid[0][2] << endl;
+  cout << "|a|\t" << grid[1][0] << "\t" << grid[1][1] << "\t" << grid[1][2] << endl;
+  cout << "|a|\t" << grid[2][0] << "\t" << grid[2][1] << "\t" << grid[2][2] << endl;
+  cout << endl;
+}
+
+//This function asks for the player's input, validates it, and adds it to the grid.
+void askInput (bool turn, int & inputRow, int & inputColumn, char grid[][3]) {
   //local variable declarations
   char input[2];
   bool validInput = false;
-
+  
+  //Name who's turn it is
+  if(turn == p1Turn) {
+    cout << "Player 1's Turn! (Player 'o')" << endl;
+  } else if(turn == p2Turn) {
+    cout << "Player 2's Turn! (Player 'x')" << endl;
+  }
+  cout << endl;
+  
   //ask for and receive input
-  cout << "please input either 'a', 'b', or 'c' for the row" << endl;
-  cout << "you want to place your symbol in, followed by " << endl;
-  cout << "'1', '2', or '3' for the column you want to " << end;
-  cout << "place your symbol in." << endl;
-  cin.get(input, 2);
+  cout << "Name the coordinates for where you want to play your symbol:" << endl;
+  cout << "Format your input like a1 or b3 (row first, then column)." << endl;
+  cin.get(input, 3);
   cin.get();
 
   //check the user's input
   while(!validInput) {
     //check for first input's validity
-    if(input[0] == 'a' || input[0] == 'b' || input[0] == 'c') {
-       if(input[1] == '1' || input[1] == '2' || input[1] == '3') {
-	 validInput = true;
-       } else {
-	 //Their second character input was invalid, so tell them!
-	 
-       }
-    } else {
-      //Their first character input was invalid, so tell them!
-      
-    }
+    if(input[0] == 'a' || input[0] == 'b' || input[0] == 'c') { //row
+      if(input[1] == '1' || input[1] == '2' || input[1] == '3') { //column
+	//Match their input to the coordinates
+	if(input[0] == 'a') { //row coordinate
+	  inputRow = 0;
+	} else if(input[0] == 'b') {
+	  inputRow = 1;
+	} else if(input[0] == 'c') {
+	  inputRow = 2;
+	}
 
+	if(input[1] == '1') { //column coordinate
+	  inputColumn = 0;
+	} else if(input[1] == '2') {
+	  inputColumn = 1;
+	} else if(input[1] == '3') {
+	  inputColumn = 2;
+	}
+
+	//Make sure the move trying to be played hasn't already been played
+	if(grid[inputRow][inputColumn] == '\0') {
+	  //Their input was fully validated, add their move to the grid
+	  if(turn == p1Turn) {
+	    grid[inputRow][inputColumn] = p1Sym;
+	  } else if(turn == p2Turn) {
+	    grid[inputRow][inputColumn] = p2Sym;
+	  }
+	  validInput = true;
+	} else { //If the move has already been played
+	  cout << "Sorry, that move has already been played." << endl;
+	  cout << "Please pick a different place to play your symbol." << endl;
+	}
+	
+      } else { //Their second character input was invalid, so tell them!
+	cout << "The second part of your input (the column input) doesn't work." << endl;
+	cout << "Please input either '1', '2', or '3' for the column" << endl;
+	cout << "you want to place your symbol in. (i.e. a1, b3)" << endl;
+      }
+      
+    } else { //Their first character input was invalid, so tell them!
+      cout << "The first part of your input (the row input) doesn't work." << endl;
+      cout << "Please input either 'a', 'b', or 'c' for the row" << endl;
+      cout << "you want to place your symbol in. (i.e. a1, b3)" << endl;
     }
   }
-
-
-  //Return checked and validated input
-  return input;
 }
