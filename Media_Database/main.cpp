7c08cb6aaf11f4cf5e887a1e45b80e2f9113976f
KevinChackoP <464801@bsd48.org>
2023-11-09 23:57:01 -0800

    Hopefully finished with media database.

--- Media_Database/main.cpp
+++ Media_Database/main.cpp
@@ -1,7 +1,12 @@
 /* 
-   This project...
+   This project creates a media database to which the user may type a command
+   in in order to do things with the media database. They may add data to the
+   database following the prompts for their inputs. They may search for data
+   from the database by title or by year of the media. Lastly, they may delete
+   data from the database also by searching by title or year of the media.
+   When they are done they can type the quit command. 
    Author: Kevin Chacko
-   Last Updated: WRITE END DATE HERE
+   Last Updated: 11/9/2023
    Period 6, C++ / Data Structures
 */
 
@@ -12,7 +17,26 @@
   I also transferred a lot of my code from my Student list project 
   since it used a lot of the same logic and structure as this project. 
 
-  WRITE MORE CITATIONS HERE
+  I got help with deleting everything marked in my vector with the
+  help of cplusplus doc's pages on the vector at function and the
+  erase function.
+  URL: https://cplusplus.com/reference/vector/vector/at/
+  URL: https://cplusplus.com/reference/vector/vector/erase/
+  The at function on vectors helped me pick out items in the
+  vector at certain indexes. The erase doc's example showed me
+  that to erase something with an index you can add that index
+  to a .begin() call on the same vector (which turns the value
+  into an iterator position).
+
+  Additionally I'd like to thank Lucas for helping me with figuring out
+  how to get cstrings into my classes. He recommended that I used char* as
+  a parameter and that I don't use any setter functions. He also told me
+  that to properly implement it into my code I'd need to use strcpy to get
+  my passed in value into my protected class value.
+
+  Lastly, I'd like to thank Mr. Galbraith for a lot of help with segFaults
+  involving virtual functions and other problems involving syntax with all
+  my classes!
 */
 
 //imports
@@ -65,7 +89,7 @@ int main() {
       //If they want to delete media from the database, do so
       cout << "Deleting media from the database." << endl;
       cout << endl;
-      //deleteMedia(mediaDatabase); ADD THIS IN WHEN IT'S READY
+      deleteMedia(mediaDatabase);
       
     } else if(commandKey == 3) {
       //If they want to search for an item in the database, do so
@@ -264,76 +288,203 @@ void addMedia(vector<digitalMedia*> & database) {
   delete director;
   delete artist;
 }
-/*
-//This function helps the user to delete media from the database
-void deleteMedia(vector<digitalMedia*> & database) {
-  //local variables for function
-  char idInput[7];
-  for(int i = 0; i < 7; i++) {
-    idInput[i] = '\0';
-  }
-  bool matchingIdFound = false;
 
-  //If the list is currently empty, don't let them delete any student
-  if(list.empty()) {
-    cout << "There isn't any students in the list to delete from " << endl;
-    cout << "the list. After you've added a few students, then " << endl;
-    cout << "consider deleting some of them from the list." << endl;
-    cout << endl;
+//This function helps the user to delete media from the database
+//using most of the same code from the search function below
+void deleteMedia(vector<digitalMedia*> & database) {
+  //If there is no media in the database, tell the user
+  if(database.empty()) {
+    cout << "Sorry, there is currently no media in the database!" << endl;
+    cout << "Please try again after data has been ADDed." << endl;
+  } else {
+    //local variables
+    int searchType = 0; //1 for title, 2 for year
+    char* title = new char[31];
+    for(int i = 0; i < 31; i++) {
+      title[i] = '\0';
+    }
+    int year = 0;
+    bool foundResult = false;
+    int searchAgain = 0;
+    vector<int> deleteMarking;
+    int markConfirm = 0;
     
-  } else { //Otherwise, continue with the rest of the function
-    //ask user for the id of the student they want to remove from the list
-    //and make sure the inputted id is valid and matches a student
-    cout << "What's the id of the student you want to remove from the list?" << endl;
-    cout << "Please make it a 6 digit integer like before (i.e. 464877)." << endl;
-    while((isdigit(idInput[0]) == 0 || isdigit(idInput[1]) == 0 || isdigit(idInput[2]) == 0 || isdigit(idInput[3]) == 0 || isdigit(idInput[4]) == 0 || isdigit(idInput[5]) == 0) || !matchingIdFound) {
-      cin.get(idInput, 7);
+    //Ask for what they want to search by in order to delete items
+    cout << "Do you want to delete items by title or year? " << endl;
+    cout << "Enter 1 for title, or 2 for year." << endl;
+    //take and validate their input asking for their preferred search attribute
+    while(searchType != 1 && searchType != 2) {
+      cin >> searchType;
       cin.ignore(numeric_limits<streamsize>::max(), '\n');
+      cout << endl;
       
-      //If the whole id isn't just numbers, let the user know they made a mistake
-      if(isdigit(idInput[0]) == 0 || isdigit(idInput[1]) == 0 || isdigit(idInput[2]) == 0 || isdigit(idInput[3]) == 0 || isdigit(idInput[4]) == 0 || isdigit(idInput[5]) == 0) {
+      //If input is invalid
+      if(searchType != 1 && searchType != 2) {
+	cout << "Sorry, that's not one of the available options." << endl;
+	cout << "Please enter 1 for title, or 2 for year." << endl;
 	cout << endl;
-	cout << "You didn't enter a 6 digit integer." << endl;
-	cout << "Could you try inputting the student's id again?" << endl;
-	cout << "(i.e. 464877, 342907)" << endl;
+      }
+    }
+
+    //Start delete loop (until they find something and verify that they want
+    //to delete it or not, or until they want to get out of delete)
+    while(!foundResult) {
+      //Ask them to give the value they want to use to find and delete
+      if(searchType == 1) { //If they are deleting by title, ask for the title
+	cout << "What is the title of the media you want to delete?" << endl;
+	cin.getline(title, 31);
 	cout << endl;
-      } else { //Is there a matching student id in the list?
-	for(vector<Student*>::iterator it = list.begin(); it != list.end(); it++) {
-	  if(strcmp(((*it) -> id), idInput) == 0) { //There's a match
-	    matchingIdFound = true;
-	    cout << "Removing student " << (*it) -> firstName << " " << (*it) -> lastName << " from list." << endl;
+	
+      } else { //If they are deleting by year, ask for the year
+	cout << "What is the release year of the media you want to delete?" << endl;
+	cout << "(positive numbers as A.D., negative as B.C.)" << endl;
+	cin >> year;
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	cout << endl;
+      }
+      
+      //Check for everything in the student list vector and print each out
+      //Before asking the user if they'd like to delete the item
+      int index = 0;
+      for(vector<digitalMedia*>::iterator it = database.begin(); it != database.end(); it++) {
+	//If search term matches with one of the corresponding fields in
+	//the classes in the media database...
+	if((searchType == 1 && strcmp((*it) -> getTitle(), title) == 0) || (searchType == 2 && (*it) -> getYear() == year)) {
+	  //If a matching title or year was found, print out whole media class
+	  //Based on the type it is
+	  if((*it) -> getID() == 1) { //If the media is a videogame
+	    //header line
+	    cout << "Video Game: " << endl;
+	    cout << "TITLE, YEAR, PUBLISHER, RATING" << endl;
+	    cout << "---------------------------------------------" << endl;
+	    //database information
+	    cout << (*it) -> getTitle() << ", ";
+	    cout << (*it) -> getYear() << ", ";
+	    cout << (*it) -> getPublisher() << ", ";
+	    cout << (*it) -> getRating() << endl;
+	    cout << "---------------------------------------------" << endl;
+	    
+	  } else if((*it) -> getID() == 2) { //If the media is music
+	    //header line
+	    cout << "Music (duration in seconds): " << endl;
+	    cout << "TITLE, YEAR, PUBLISHER, ARTIST, DURATION" << endl;
+	    cout << "---------------------------------------------" << endl;
+	    //database information
+	    cout << (*it) -> getTitle() << ", ";
+	    cout << (*it) -> getYear() << ", ";
+	    cout << (*it) -> getPublisher() << ", ";
+	    cout << (*it) -> getArtist() << ", ";
+	    cout << (*it) -> getDuration() << endl;
+	    cout << "---------------------------------------------" << endl;
+	    
+	  } else if((*it) -> getID() == 3) { //If the media is a movie
+	    //header line
+	    cout << "Movie (duration in minutes): " << endl;
+	    cout << "TITLE, YEAR, DIRECTOR, RATING, DURATION" << endl;
+	    cout << "---------------------------------------------" << endl;
+	    //database information
+	    cout << (*it) -> getTitle() << ", ";
+	    cout << (*it) -> getYear() << ", ";
+	    cout << (*it) -> getDirector() << ", ";
+	    cout << (*it) -> getRating() << ", ";
+	    cout << (*it) -> getDuration() << endl;
+	    cout << "---------------------------------------------" << endl;
+	  }
+
+	  //note that a result was found and function was sucessful
+	  foundResult = true;
+
+	  //ask the user if they'd like to delete the item
+	  cout << "Are you sure you want to delete this media?" << endl;
+	  cout << "Enter 1 for yes, or 2 for no." << endl;
+	  //take and validate their input
+	  while(markConfirm != 1 && markConfirm != 2) {
+	    cin >> markConfirm;
+	    cin.ignore(numeric_limits<streamsize>::max(), '\n');
 	    cout << endl;
+	    
+	    //If input is invalid
+	    if(searchType != 1 && searchType != 2) {
+	      cout << "Sorry, that's not one of the available options." << endl;
+	      cout << "Please enter 1 for yes, or 2 for no." << endl;
+	      cout << endl;
+	    }
+	  }
+	  //based on their input delete or don't delete the item
+	  if(markConfirm == 1) { //if they said yes, mark item for deletion
+	    deleteMarking.push_back(index);
+	  }
+
+	  //change variables for next loop
+	  markConfirm = 0;
+	  index++;
+	}
+      }
+
+      //If no results were found
+      if(!foundResult) {
+	//Ask them if they'd like to try deleting again
+	cout << "Sorry, no results were found. Would you like to " << endl;
+	cout << " try again with a different search term?" << endl;
+	cout << "Enter 1 for yes, or 2 for no." << endl;
+
+	//validate their answer input
+	while(searchAgain != 1 && searchAgain != 2) {
+	  cin >> searchAgain;
+	  cin.ignore(numeric_limits<streamsize>::max(), '\n');
 	  
-	    //get rid of the student from the vector
-	    delete *it;
-	    list.erase(it);
-	    break;
+	  //If input is invalid
+	  if(searchAgain != 1 && searchAgain != 2) {
+	    cout << endl;
+	    cout << "Sorry, that's not one of the available options." << endl;
+	    cout << "Please enter 1 to search again, or 2 to exit." << endl;
+	    cout << endl;
 	  }
 	}
 
-	//If after going through the whole vector and no match could be found
-	//let the user know that their id doesn't work before letting them
-	//try again
-	if(!matchingIdFound) {
-	  cout << "Sorry, I couldn't find a student with that id in the list." << endl;
-	  cout << "This is what your current list is like" << endl;
-	  //Show the user their current list so they can see the right id
-	  //of who they want to delete off the list
-	  cout << endl;
-	  printList(list);
-	  cout << "Could you reinput the id of the student you want " << endl;
-	  cout << "to remove from the list?" << endl;
-	  cout << endl;
+	//Based on their input, continue search or exit
+	if(searchAgain == 1) { //if they enter yes, go to start of loop again
+	  //reset variables
+	  searchAgain = 0;
+	  for(int i = 0; i < 31; i++) {
+	    title[i] = '\0';
+	  }
+	  year = 0;
+	} else { //if the say no, exit the function
+	  foundResult = true;
+	}
+      } else { //If a result was found, delete all the marked items
+	while(!deleteMarking.empty()) {
+	  /*
+	    I got help with deleting everything marked in my vector with the
+	    help of cplusplus doc's pages on the vector at function and the
+	    erase function.
+	    URL: https://cplusplus.com/reference/vector/vector/at/
+	    URL: https://cplusplus.com/reference/vector/vector/erase/
+	    The at function on vectors helped me pick out items in the
+	    vector at certain indexes. The erase doc's example showed me
+	    that to erase something with an index you can add that index
+	    to a .begin() call on the same vector (which turns the value
+	    into an iterator position).
+	   */
+	  delete database.at(deleteMarking.back());
+	  database.erase(database.begin() + deleteMarking.back());
+	  deleteMarking.pop_back();
 	}
       }
     }
+
+    //delete heap data pointed to made in function
+    delete title;
   }
-}*/
+  
+  cout << endl;
+}
 
 //This function helps the user to search for media in the database and print
 //out media that matches with their search (based on title or year released)
 void searchDatabase(vector<digitalMedia*> & database) {
-  //If there is nobody in the database, tell the user
+  //If there is no media in the database, tell the user
   if(database.empty()) {
     cout << "Sorry, there is currently no media in the database!" << endl;
     cout << "Please try again after data has been ADDed." << endl;
