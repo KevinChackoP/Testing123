40569e501a1dacdddb8ce64b9293bb5c24538fe7
U-BSD\464801 <464801@SHS-75W2NW3.beaverton.k12.or.us>
2023-11-14 12:24:02 -0800

    Starting structure for main.cpp

---
+++ Zuul/main.cpp
@@ -0,0 +1,48 @@
+/* 
+   This program... WRITE MORE HERE, MAKE SURE TO INCLUDE MAP OF ZUUL GAME IN
+   THIS PROGRAM'S FOLDER!
+   Author: Kevin Chacko
+   Last Updated: WRITE END DATE HERE
+   Period 6, C++ / Data Structures
+*/
+
+/*
+  NOTE CITATIONS HERE
+*/
+
+//imports
+#include <iostream>
+#include <cstring>
+#include <cctype>
+#include <ios>
+#include <limits>
+#include <math.h>
+#include <vector>
+#include <map>
+
+//class imports
+#include "room.h"
+
+using namespace std;
+
+//Function prototypes
+void instructions();
+
+//Start of main function
+int main() {
+  //local variable declarations
+  vector<room*> roomList;
+  bool inUse = true;
+  int commandKey = 0;
+
+  //Tell the user how the program works
+  instructions();
+  
+  //If nothing went wrong, return 0
+  return 0;
+}
+
+//This function tells the user how to use the program
+void instructions() {
+  //ADD INSTRUCTIONS HERE
+}
