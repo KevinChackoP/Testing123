37fcc3c1abc356e41649c10709ba94b6d6956813
U-BSD\464801 <464801@SHS-GYBF6X3.beaverton.k12.or.us>
2024-02-14 12:25:04 -0800

    Hopefully finished manual adding function, moving onto file adding

--- Heap/main.cpp
+++ Heap/main.cpp
@@ -45,11 +45,11 @@ const int emptyNode = -1;
 //Function prototypes
 void instructions();
 int askCommand();
-void addNodesManually(int* & tree);
-void addNodesByFile(int* & tree);
-void addNode(int input, int* & tree);
-void removeNode(int* & tree);
-void removeAll(int* & tree);
+void addNodesManually(int* tree);
+void addNodesByFile(int* tree);
+void addNode(int input, int* tree, bool & full);
+void removeNode(int* tree);
+void removeAll(int* tree);
 void printTree(int* tree);
 
 //Start of main function
@@ -57,9 +57,9 @@ int main() {
   //local variable declarations
   bool inUse = true;
   int commandKey = 0;
-  int heap[100] = new int[100];
+  int heap[100];
   for(int i = 0; i < 100; i++) {
-    //Set everything in the heap to null
+    //Set everything in the heap to the empty node number
     heap[i] = emptyNode;
   }
 
@@ -103,7 +103,7 @@ int main() {
       //do so
       cout << "Printing out the current tree as it is." << endl;
       cout << endl;
-
+      printTree(heap);
       
     } else if(commandKey == 6) {
       //If they want to quit the program, do so
@@ -124,6 +124,115 @@ void instructions() {
   cout << "Hello, this is a program helps you make a heap of 100 numbers " << endl;
   cout << "between 1-1000 (inclusive). You can use various commands to interact" << endl;
   cout << "with the heap binary search tree. You can interact with the binary" << endl;
-  cout << "search tree by... WRITE WHAT YOU CAN DO WITH THE PROGRAM HERE!!!" << endl;
+  cout << "search tree by manually adding numbers to the tree, using a file " << endl;
+  cout << "with various numbers between 1-1000 seperated by spaces to add to " << endl;
+  cout << "the tree, removing and viewing the root node of the tree, removing " << endl;
+  cout << "all the nodes of the tree and seeing them in order of greatest to " << endl;
+  cout << "least, and you can print out the tree to visually see its current " << endl;
+  cout << "structure. I hope this program is of use to you!" << endl;
   cout << endl;
+}
+
+//This function asks the user for their command
+int askCommand() {
+  //make an input array and clean it up
+  char input[10];
+  for(int i = 0; i < 10; i++) {
+    input[i] = '\0';
+  }
+
+  //Tell them how to input a command and what each of the commands do
+  cout << "Please input a command to tell me what you want to do." << endl;
+  cout << "Input ADDM to add numbers to the list manually." << endl;
+  cout << "Input ADDF to add numbers to the list via a file." << endl;
+  cout << "Input REM to remove the root node and print it out." << endl;
+  cout << "Input REMALL to remove all the nodes and print them in " << endl;
+  cout << "order from greatest to least." << endl;
+  cout << "Input PRINT to print out the tree's structure visually." << endl;
+  cout << "Input QUIT to exit the program." << endl;
+  cout << endl;
+
+  //ask them for a command input, if the input is valid return an int for the
+  //command key, if invalid have them reinput their desired command.
+  while(true) {
+    //Ask for input
+    cin.getline(input, 10);
+
+    //Clean input up by making it all upper case
+    for(int i = 0; i < 10; i++) {
+      input[i] = toupper(input[i]);
+    }
+
+    //return the corresponding command key to their command
+    if(strcmp(input, "ADDM") == 0) {
+      return 1;
+    } else if(strcmp(input, "ADDF") == 0) {
+      return 2;
+    } else if(strcmp(input, "REM") == 0) {
+      return 3;
+    } else if(strcmp(input, "REMALL") == 0) {
+      return 4;
+    } else if(strcmp(input, "PRINT") == 0) {
+      return 5;
+    } else if(strcmp(input, "QUIT") == 0) {
+      return 6;
+    } else { //If their command is invalid, tell them and have them try again
+      cout << "Sorry, I don't recognize that command." << endl;
+      cout << "Please input one of the following commands: " << endl;
+      cout << "ADDM, ADDF, REM, REMALL, PRINT, QUIT." << endl;
+      cout << endl;
+    }
+  }
+}
+
+//This function will add nodes into the tree via manual input
+void addNodesManually(int* tree) {
+  //local variables
+  int numInput = 0;
+  bool treeFull = false;
+
+  //Tell them what to input
+  cout << "Please input an integer number between 1-1000 (inclusive)." << endl;
+  cout << "If you wish to add multiple numbers, put your numbers in " << endl;
+  cout << "a string with only spaces between each of your numbers." << endl;
+  cout << "In both cases, after you're done typing all the numbers " << endl;
+  cout << "you want to add, click enter and then type and enter " << endl;
+  cout << "\"-1\" to signal that you're finished." << endl;
+  cout << endl;
+
+  //read through their input and add their numbers to the tree
+  while((cin >> numInput) && numInput != -1 && !(treeFull)) {
+    addNode(numInput, tree, treeFull);
+  }
+
+  //do some clean up
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  cout << endl;
+}
+
+//This function will add nodes into the tree via file input
+void addNodesByFile(int* tree) {
+  
+}
+
+//This function will take the input and actually put it into the tree properly
+void addNode(int input, int* tree, bool & full) {
+  cout << input << endl;
+  cout << endl;
+}
+
+//This function will remove the root node of the tree and print it out
+void removeNode(int* tree) {
+
+}
+
+//This function will remove all the nodes of the tree and print them out
+//in order of greatest to least
+void removeAll(int* tree) {
+
+}
+
+//This function will print out a visual representation of the tree
+void printTree(int* tree) {
+
 }
